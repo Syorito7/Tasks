35a300af33a49d4075cca53de9f019278e3b5317
Nikitos <nikitosik.200210@gmail.com>
2025-05-31 19:57:28 +0300

    added CMakeLists for 3-11 laba and main CMakeLists

---
+++ HSE/Course_1/C/Lab_Type_№3/CMakeLists.txt.cmake
@@ -0,0 +1,11 @@
+cmake_minimum_required(VERSION 3.10)
+
+# Название проекта
+project(Lab_type_3)
+
+# Установка стандарта C++
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED True)
+
+# Добавление исполняемого файла
+add_executable(Lab_type_3 "Lab_type_№3.cpp")
